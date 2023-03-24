f8edb5b04cfcd5c09af5ecb90e55acf6268350ac
Roger Korpics <rmk347@drexel.edu>
2023-03-24 17:20:47 -0400

    Improved button vote randomization so that the buttons cannot draw the same label as the previous round

--- idm362-rmk347/VotingViewController.swift
+++ idm362-rmk347/VotingViewController.swift
@@ -18,6 +18,9 @@ class VotingViewController: UIViewController {
 
     var chooseColumn = 0
     var chooseRow = 0
+    
+    var lastRowDrawn: Int?
+    var lastColumnDrawn: Int?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,17 +59,25 @@ class VotingViewController: UIViewController {
     }
     
     func drawRound(){ //This function creates a pair up between two items.
-        
-        chooseRow = Int.random(in: 0...(votingListItems.count - 1)) //First it draws the row
-        
+       
+        repeat{
+            chooseRow = Int.random(in: 0...(votingListItems.count - 1)) //First it draws the row
+        } while (chooseRow == lastRowDrawn)
+            
         repeat { //Then it will draw the column. Note how it always redraws if the column is the same as the row. This will prevent the system from choosing pair-ups between the same two objects.
         chooseColumn = Int.random(in: 0...(votingListItems.count - 1))
-        } while (chooseColumn == chooseRow)
+        } while ((chooseColumn == lastColumnDrawn) || (chooseColumn == chooseRow))
         
         print(chooseRow)
         
-        LeftButtonUI.setTitle(String(votingListItems[chooseColumn]), for: .normal)
-        RightButtonUI.setTitle(String(votingListItems[chooseRow]), for: .normal)
+        //Sets the last row drawn to new variables
+        lastRowDrawn = chooseRow
+        lastColumnDrawn = chooseColumn
+        
+        
+    
+        LeftButtonUI.setTitle(String(votingListItems[chooseColumn]), for: .normal) //Changes the label for the left button
+        RightButtonUI.setTitle(String(votingListItems[chooseRow]), for: .normal) //Changes the label for the right button
         
 
     }
